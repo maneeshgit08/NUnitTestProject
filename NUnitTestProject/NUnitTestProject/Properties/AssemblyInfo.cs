27ed35b02f382f0d5c97d1ca9c00f07aa5092c0e
Kabra, Manish (CT RDA DS AA DTS AU APPS) <manish.kabra@siemens.com>
2020-08-16 19:51:33 +0530

    first commit

---
+++ NUnitTestProject/NUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9056c4a-8d51-4419-9f72-3429f04ae690")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NUnitTestProject/NUnitTestProject/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitTestProject
+{
+    public class UnitTest1
+    {
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            Console.WriteLine("One time setup");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Console.WriteLine("Teardown");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Console.WriteLine("One time teardown");
+        }
+
+        [TestCase]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Test Method 1");
+        }
+    }
+}
